2817febf4ef65f916e7a78f690ae776093bcc16b
Ayshwarya Jaiswal <44940768+ayshwaryaj@users.noreply.github.com>
2026-02-15 14:51:35 +0530

    Time: 181 ms (5.03%), Space: 47.1 MB (13.13%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -0,0 +1,30 @@
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+//        if (list1 == null) return list2;
+//        if (list2 == null) return list1; 
+        if(lists.length==0)
+            return null;
+        else if(lists.length == 1)
+            return lists[0];
+        else{
+        ListNode MergedList = lists[0];
+        for(int i = 1; i<lists.length; i++)
+        {
+            ListNode list2 = lists[i];
+            MergedList = MKL(MergedList, list2);
+        }
+        return MergedList;}
+    }
+    private ListNode MKL(ListNode list1, ListNode list2)
+    {
+        if(list1 == null) return list2;
+        if(list2 == null) return list1;
+        if (list1.val <= list2.val) {
+            list1.next = MKL(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = MKL(list1, list2.next);
+            return list2;
+        }
+    }
+}
