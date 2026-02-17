28287dd2fee55aa229c97e16d4ea3fd9c8eb3629
Ayshwarya Jaiswal <44940768+ayshwaryaj@users.noreply.github.com>
2026-02-17 12:32:49 +0530

    Time: 1 ms (99.65%), Space: 77.5 MB (18.67%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 
     public int maxSubArray(int[] nums) {
         return msa(nums, 0, nums.length - 1);
@@ -36,5 +36,20 @@ class Solution {
         }
 
         return leftSum + rightSum;
+    }
+}*/
+
+class Solution {
+
+    public int maxSubArray(int[] nums) {
+        int CS = 0, MS = Integer.MIN_VALUE;
+        for(int i=0; i<nums.length; i++)
+        {
+            CS += nums[i];
+            MS = Math.max(CS,MS);
+            if(CS < 0)
+                CS=0;
+        }
+        return MS;
     }
 }
