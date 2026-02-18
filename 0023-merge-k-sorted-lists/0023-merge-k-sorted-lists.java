e3ab0d3a395a78934b0623af96db8bc014fac48c
Ayshwarya Jaiswal <44940768+ayshwaryaj@users.noreply.github.com>
2026-02-18 10:23:58 +0530

    Time: 2 ms (89.37%), Space: 46.9 MB (44.98%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         if(lists.length==0)
             return null;
@@ -22,6 +22,41 @@ class Solution {
             return list1;
         } else {
             list2.next = MKL(list1, list2.next);
+            return list2;
+        }
+    }
+}
+
+
+upon Optimizing */
+
+class Solution {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) return null;
+        return divide(lists, 0, lists.length - 1);
+    }
+
+    private ListNode divide(ListNode[] lists, int left, int right) {
+        if (left == right) return lists[left];
+
+        int mid = left + (right - left) / 2;
+
+        ListNode l1 = divide(lists, left, mid);
+        ListNode l2 = divide(lists, mid + 1, right);
+
+        return merge(l1, l2);   
+    }
+
+    private ListNode merge(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        if (list1.val <= list2.val) {
+            list1.next = merge(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = merge(list1, list2.next);
             return list2;
         }
     }
