ea73824f280fcc3f082e23635cc456e56c23e1e4
Ayshwarya Jaiswal <44940768+ayshwaryaj@users.noreply.github.com>
2026-02-18 10:30:37 +0530

    Time: 2 ms (89.37%), Space: 47.2 MB (13.29%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -30,6 +30,15 @@
 
 upon Optimizing */
 
+/* 
+*
+*
+*
+Space complexity is high 
+*
+*
+*
+
 class Solution {
 
     public ListNode mergeKLists(ListNode[] lists) {
@@ -59,5 +68,63 @@ class Solution {
             list2.next = merge(list1, list2.next);
             return list2;
         }
+    }
+}
+ 
+*
+*
+*
+Reducing Space complexity with iterative technique
+*
+*
+*
+*/
+
+class Solution {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) return null;
+
+        int interval = 1;
+
+        while (interval < lists.length) {
+            for (int i = 0; i + interval < lists.length; i += interval * 2) {
+                lists[i] = merge(lists[i], lists[i + interval]);
+            }
+            interval *= 2;
+        }
+
+        return lists[0];
+    }
+
+    private ListNode merge(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        ListNode head;
+        if (l1.val <= l2.val) {
+            head = l1;
+            l1 = l1.next;
+        } else {
+            head = l2;
+            l2 = l2.next;
+        }
+
+        ListNode tail = head;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+
+        tail.next = (l1 != null) ? l1 : l2;
+
+        return head;
     }
 }
