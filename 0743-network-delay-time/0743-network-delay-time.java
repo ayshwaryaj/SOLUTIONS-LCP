a6ee80efaa09c497c985c755622ae519fe3c0830
Ayshwarya Jaiswal <44940768+ayshwaryaj@users.noreply.github.com>
2026-03-22 19:41:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
         ArrayList<ArrayList<int[]>> adj = new ArrayList<>();
         for(int i=0; i<n; i++)
@@ -26,7 +26,7 @@ class Solution {
                 return a[1]-b[1];
             }
         });*/
-        int time[]=new int[n];
+/*        int time[]=new int[n];
         Arrays.fill(time, Integer.MAX_VALUE);
         time[s] = 0;
         pq.offer(new int[]{s,0});
@@ -48,5 +48,38 @@ class Solution {
             }
         }
         return time;
+    }
+}*/
+class Solution {
+    public int networkDelayTime(int[][] times, int n, int k) {
+    Map<Integer, List<int[]>> graph = new HashMap<>();
+    for(int[] time: times)
+    {
+        int src = time[0], tar = time[1], weight=time[2];
+        if(!graph.containsKey(src))
+            graph.put(src,new LinkedList<int[]>());
+        graph.get(src).add(new int[] {tar, weight});
+    }
+    PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b)->a[1]-b[1]);
+    Set<Integer> visited = new HashSet<>();
+    minHeap.add(new int[]{k,0});
+    int res = 0;
+    while(!minHeap.isEmpty())
+    {
+        int[] top = minHeap.poll();
+        int src = top[0], srcWeight = top[1];
+        if(visited.contains(src))
+            continue;
+        res=srcWeight;
+        visited.add(src);
+        if(!graph.containsKey(src))
+            continue;
+        for(int[] edge:graph.get(src))
+        {
+            int tar = edge[0], tarweight = edge[1];
+            minHeap.add(new int[]{tar, srcWeight+tarweight});
+        }
+    }
+    return visited.size()==n?res:-1;
     }
 }
