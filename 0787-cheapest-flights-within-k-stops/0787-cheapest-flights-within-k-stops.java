5326607b94edb656c6583b1683bbac39638d4fa3
Ayshwarya Jaiswal <44940768+ayshwaryaj@users.noreply.github.com>
2026-03-22 22:24:18 +0530

    Time: 5 ms (98.39%), Space: 46.9 MB (13.48%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        
+        // Step 1: Initialize distance array with a large value
+        int[] prices = new int[n];
+        Arrays.fill(prices, Integer.MAX_VALUE);
+        prices[src] = 0;
+
+        for (int i = 0; i <= k; i++) {
+            
+            int[] tmpPrices = Arrays.copyOf(prices, n);
+            
+            for (int[] flight : flights) {
+                int u = flight[0]; 
+                int v = flight[1]; 
+                int price = flight[2];
+                
+                if (prices[u] != Integer.MAX_VALUE) {
+                    if (prices[u] + price < tmpPrices[v]) {
+                        tmpPrices[v] = prices[u] + price;
+                    }
+                }
+            }
+            prices = tmpPrices;
+        }
+
+        return prices[dst] == Integer.MAX_VALUE ? -1 : prices[dst];
+    }
+}
+
+        
+    
