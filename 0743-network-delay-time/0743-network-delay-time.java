274be50cc24e2b46322decfe53f9c6b00c0d936f
Ayshwarya Jaiswal <44940768+ayshwaryaj@users.noreply.github.com>
2026-03-22 16:53:25 +0530

    Time: 15 ms (35.27%), Space: 48.5 MB (95.12%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int networkDelayTime(int[][] times, int n, int k) {
+        ArrayList<ArrayList<int[]>> adj = new ArrayList<>();
+        for(int i=0; i<n; i++)
+            adj.add(new ArrayList<>());
+        for(int time[]:times)
+        {
+            int u = time[0]-1;
+            int v = time[1]-1;
+            int w = time[2];
+            adj.get(u).add(new int[]{v,w});
+        }
+        int minTime[] = dijkstra(k-1, n, adj);
+        int res = Integer.MIN_VALUE;
+        for(int time: minTime)
+            res = Math.max(res,time);
+        return(res == Integer.MAX_VALUE)?-1:res;
+    }
+    public int[] dijkstra(int s, int n, ArrayList<ArrayList<int[]>> adj)
+    {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[1]-b[1]);
+        
+        /*(new Comparator<int[]>(){
+            public int compare(int a[], int b[])
+            {
+                return a[1]-b[1];
+            }
+        });*/
+        int time[]=new int[n];
+        Arrays.fill(time, Integer.MAX_VALUE);
+        time[s] = 0;
+        pq.offer(new int[]{s,0});
+        while(!pq.isEmpty()){
+            int pair[]=pq.poll();
+            int u =pair[0];
+            int w = pair[1];
+            if(w>time[u])
+                continue;
+            for(int neighbor[]:adj.get(u))
+            {
+                int v = neighbor[0];
+                int t = neighbor[1];
+                if(time[u]+t<time[v])
+                {
+                    time[v]=time[u]+t;
+                    pq.offer(new int[]{v,time[v]});
+                }
+            }
+        }
+        return time;
+    }
+}
