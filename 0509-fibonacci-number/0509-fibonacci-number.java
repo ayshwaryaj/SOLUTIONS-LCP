bbeee80a2e242754b060c824a7fb9a76ff19fa06
Ayshwarya Jaiswal <44940768+ayshwaryaj@users.noreply.github.com>
2026-03-30 15:41:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,7 +1,21 @@
 class Solution {
     public int fib(int n) {
+        int dp[] = new int[n+1];
+        Arrays.fill(dp, -1);
+        return recur(n, dp);
+    }
+    public int recur(int n, int dp[])
+    {
         if(n<=1)
-            return n;
-        return fib(n-1)+fib(n-2);
+        {
+            dp[n]=n;
+            return dp[n];
+        }
+        if(dp[n]!=-1)
+        {
+            return dp[n];
+        }
+        dp[n] = dp[n-1]+dp[n-2];
+        return dp[n];
     }
 }
