59d526ff3638c6b9fd1b8a9e33fa9c491871897b
Ayshwarya Jaiswal <44940768+ayshwaryaj@users.noreply.github.com>
2026-04-03 12:48:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,12 +1,16 @@
 class Solution {
     public int fib(int n) {
-        int dp[] =new int[n+1];
         if(n<=1)
             return n;
-        dp[0] = 0;
-        dp[1] = 1;
+        int p2 = 0;
+        int p1 = 1;
+        int ans = 0;
         for(int s = 2; s<=n; s++)
-            dp[s] = dp[s-1]+dp[s-2];
-        return dp[n];
+            {
+                ans = p1+p2;
+                p2=p1;
+                p1=ans;
+            }
+        return ans;
     }
 }
