d0c33085b3c8dfbb7eb54906f09fb072da6ca425
Ayshwarya Jaiswal <44940768+ayshwaryaj@users.noreply.github.com>
2026-04-06 10:40:21 +0530

    Time: 0 ms (100%), Space: 41.8 MB (88.44%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -3,9 +3,9 @@ class Solution {
         int dp[] = new int [n+1];
         if(n<=1)
             return n;
-        p2 = 0;
-        p1 = 1;
-        int ans;
+        int p2 = 0;
+        int p1 = 1;
+        int ans = 0;
         for(int s=2; s<=n; s++)
             {
                 ans = p1+p2;
