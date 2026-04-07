50199f578c3fd36097db28769461af6052fa7c9e
Ayshwarya Jaiswal <44940768+ayshwaryaj@users.noreply.github.com>
2026-04-07 11:18:46 +0530

    Time: 0 ms (100%), Space: 42 MB (58.93%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -3,15 +3,10 @@ class Solution {
         int dp[] = new int [n+1];
         if(n<=1)
             return n;
-        int p2 = 0;
-        int p1 = 1;
-        int ans = 0;
+        dp[0] = 0;
+        dp[1] = 1;
         for(int s=2; s<=n; s++)
-            {
-                ans = p1+p2;
-                p2 = p1;
-                p1 = ans;
-            }
-        return ans;
+            dp[s] = dp[s-1]+dp[s-2];
+        return dp[n];
     }
 }
