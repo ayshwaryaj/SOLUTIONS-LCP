703ea956603abe868c8b96d5d16d1f4cc7c006d1
Ayshwarya Jaiswal <44940768+ayshwaryaj@users.noreply.github.com>
2026-04-09 12:56:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int maximalSquare(char[][] matrix) {
         int ms= 0;
         int [][]m = new int [matrix.length][matrix[0].length]; 
@@ -24,6 +24,42 @@ class Solution {
             }
         }
         System.out.print(ms);
+        return ms * ms;
+    }
+}
+*/
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int ms = 0;
+
+        // normalize input
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[i][j] = (char)(matrix[i][j] - '0');
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+
+                if (matrix[i][j] == 1) {
+
+                    if (i > 0 && j > 0) {
+                        int T = matrix[i-1][j];
+                        int TL = matrix[i-1][j-1];
+                        int L = matrix[i][j-1];
+
+                        int currentSide = 1 + Math.min(Math.min(T, TL), L);
+                        matrix[i][j] = (char) currentSide;
+
+                        ms = Math.max(ms, currentSide);
+                    } else {
+                        ms = Math.max(ms, 1);
+                    }
+                }
+            }
+        }
+
         return ms * ms;
     }
 }
