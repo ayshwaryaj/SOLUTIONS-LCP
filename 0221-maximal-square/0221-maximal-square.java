4caca36aa51a5e7ebe1a17b83c4dc18f3131a41b
Ayshwarya Jaiswal <44940768+ayshwaryaj@users.noreply.github.com>
2026-04-09 12:56:05 +0530

    Time: 11 ms (15.93%), Space: 71.2 MB (30.8%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -1,4 +1,4 @@
-/*class Solution {
+class Solution {
     public int maximalSquare(char[][] matrix) {
         int ms= 0;
         int [][]m = new int [matrix.length][matrix[0].length]; 
@@ -26,42 +26,4 @@
         System.out.print(ms);
         return ms * ms;
     }
-}*/
-
-class Solution {
-    public int maximalSquare(char[][] matrix) {
-        int ms = 0;
-
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-
-                if (matrix[i][j] == '1' || matrix[i][j] == 1) {
-
-                    if (i > 0 && j > 0) {
-
-                        int T = getVal(matrix[i-1][j]);
-                        int TL = getVal(matrix[i-1][j-1]);
-                        int L = getVal(matrix[i][j-1]);
-
-                        int currentSide = 1 + Math.min(Math.min(T, TL), L);
-
-                        matrix[i][j] = (char) currentSide; // store raw
-
-                        ms = Math.max(ms, currentSide);
-                    } else {
-                        matrix[i][j] = 1; // convert edge '1' → 1
-                        ms = Math.max(ms, 1);
-                    }
-                }
-            }
-        }
-
-        return ms * ms;
-    }
-
-    private int getVal(char c) {
-        if (c == '0' || c == '1') return c - '0';
-        return c;
-    }
 }
-
