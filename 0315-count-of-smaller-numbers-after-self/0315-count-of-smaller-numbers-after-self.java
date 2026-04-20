8f6334a6684fa6f12f640ca9ec27cfe4a20ebdde
Ayshwarya Jaiswal <44940768+ayshwaryaj@users.noreply.github.com>
2026-04-20 22:57:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.java
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.java
@@ -1,4 +1,59 @@
 class Solution {
+    public List<Integer> countSmaller(int[] nums) 
+    {
+        List<Integer> smallerToRight = new LinkedList<>();
+        List<Integer> sortedList = new ArrayList<>();
+        for(int i = nums.length-1; i>=0; i--)
+        {
+            int index = insertAndGetIndex(sortedList, nums[i]);
+            smallerToRight.add(0,index);
+        }
+        return smallerToRight;
+    }
+    private int insertAndGetIndex(List<Integer> sortedList, int num)
+    {
+        int low = 0;
+        int high = sortedList.size()-1;
+        if(sortedList.isEmpty() || num<sortedList.get(0))
+        {
+            sortedList.add(0,num);
+            return 0;
+        }
+        if(num>sortedList.get(high))
+        {
+            sortedList.add(num);
+            return high+1;
+        }
+        while(high-low > 1)
+        {
+            int mid = (low+high)/2;
+            if(num<=sortedList.get(mid))
+            {
+                high = mid;
+            }
+            else
+            {
+                low=mid;
+            }
+        }
+            if(num<=sortedList.get(low))
+            {
+                sortedList.add(low, num);
+                return low;
+            }
+            else if(num> sortedList.get(high))
+            {
+                sortedList.add(high+1, num);
+                return high+1;
+            }
+            else
+            {
+                sortedList.add(high, num);
+                return high;
+            }
+        }
+    }
+/*class Solution {
     int [] count;
     public List<Integer> countSmaller(int[] nums) {
        List<Integer>result = new ArrayList<>();
@@ -73,4 +128,4 @@ class Solution {
             k++;
         }
     }
-}
+}*/
